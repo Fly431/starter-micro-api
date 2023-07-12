39762b5cb667c5e043bab5b2b80ca8806a668333
Fly431 <139393396+Fly431@users.noreply.github.com>
2023-07-12 22:34:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    "URL_SERVER": "0.0.0.0:30120", // IP:port
-    "URL_CFX": "", // "https://cfx.re/join/xxxx" or "" for use URL_SERVER. Using cfx.re It only contains online player information and server names.
-    "SERVER_NAME": "Name server",
-    "BOT_TOKEN": "", // https://discord.com/developers/applications
+    "URL_SERVER": "37.138.70.250:30110", // IP:port
+    "URL_CFX": "cfx.re/join/kzgyz6", // "https://cfx.re/join/xxxx" or "" for use URL_SERVER. Using cfx.re It only contains online player information and server names.
+    "SERVER_NAME": "24/seven Roleplay",
+    "BOT_TOKEN": "MTEyODc4NTAwOTcxNzgwOTI4NA.GVJsnx.csA41NH71E4kSMiY7J2TMZ8_caJt7yw4tqc0c4", // https://discord.com/developers/applications
     "SERVER_LOGO": "https://www.catdumb.com/wp-content/uploads/2022/04/2-75.jpg",
     "COLORBOX": "Blue", // White, Aqua, Green, Blue, Yellow, Purple, LuminousVividPink, Fuchsia, Gold, Orange, Red, Grey, Navy, DarkAqua, DarkGreen, DarkBlue, DarkPurple, DarkVividPink, DarkGold, DarkOrange, DarkRed, DarkGrey, DarkerGrey, LightGrey, DarkNavy, Blurple, Greyple, DarkButNotBlack, NtQuiteBlack, Random
     "NAMELIST": "police", // name for count
